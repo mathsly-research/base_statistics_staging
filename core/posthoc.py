eec45822319c52a3981586eb127b647566a025bc
mathsly-research <marilu.garo@gmail.com>
2025-08-27 09:54:48 +0200

    Create posthoc.py

---
+++ core/posthoc.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+
+try:
+    import statsmodels.stats.multicomp as mc
+except Exception:
+    mc = None
+
+try:
+    import scikit_posthocs as sp posthocs
+except Exception:
+    posthocs = None
+
+
+def tukey_hsd(data: pd.Series, group: pd.Series):
+    """
+    Tukey HSD post-hoc dopo ANOVA.
+    Ritorna un DataFrame con confronti a coppie.
+    """
+    if mc is None:
+        return None
+    df = pd.DataFrame({"y": data, "g": group}).dropna()
+    comp = mc.MultiComparison(df["y"], df["g"])
+    res = comp.tukeyhsd()
+    return pd.DataFrame(data=res._results_table.data[1:], columns=res._results_table.data[0])
+
+
+def dunn_test(data: pd.Series, group: pd.Series, p_adjust="bonferroni"):
+    """
+    Dunn post-hoc dopo Kruskal–Wallis.
+    """
+    if posthocs is None:
+        return None
+    df = pd.DataFrame({"y": data, "g": group}).dropna()
+    res = posthocs.posthoc_dunn(df, val_col="y", group_col="g", p_adjust=p_adjust)
+    return res
